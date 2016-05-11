6d0e014c9fb2571a45f3558fa35eed7c1b42c371
hubysh <design.ysh@gmail.com>
2016-05-11 20:12:41 +0900

    초기화

---
+++ Assets/03_Scripts/Scripts_Common/scpSingleton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 전역에서 접근 가능한 클래스
+/// MonoBehaviour을 상속한 이유는 Inspetor에서 정보를 실시간으로 보기 위함
+/// 상속 없을 경우, 객체 핸들 얻지 못함.
+/// </summary>
+public class Singleton : MonoBehaviour
+{
+	#region Singleton test1
+	private static Singleton instance = null;
+
+	public GameObject selectProjectWindow;
+	public GameObject projectDropdown;
+
+
+	public static Singleton Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				GameObject objSingleton = new GameObject("objSingleton");
+				instance = objSingleton.AddComponent(typeof(Singleton)) as Singleton;
+			}
+			return instance;
+		}
+	}
+
+	void Awake()
+	{
+		selectProjectWindow = GameObject.Find("selectProjectWindow_panel");
+
+		projectDropdown = GameObject.Find("project_dropdown");
+
+	}
+	#endregion
+
+
+}
